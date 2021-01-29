be16cb49d62d31ca53054fc9a839829d459408e0
Rawat, Narendra Singh <narendrasingh.rawat@nobelbiocare.com>
2021-01-29 12:01:46 +0530

    RoundHouse Db Migration

---
+++ src/Database.Migration/ConnectionStringHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Database.Migration
+{
+    public static class ConnectionStringHelper
+    {
+        public static string GetConnectionString(string connectionStringKey)
+        {
+            var connKey = string.Format("{0}_{1}", connectionStringKey, Environment.MachineName);
+            var setting = System.Configuration.ConfigurationManager.ConnectionStrings[connKey];
+            if (setting == null)
+                throw new InvalidOperationException(string.Format("Connection string for key '{0}' is missing in config file.", connKey));
+            return setting.ConnectionString;
+        }
+    }
+}
---
+++ src/Database.Migration/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace Database.Migration
+{
+    class Program
+    {
+        private static readonly string rootDirectory = Path.GetDirectoryName(Assembly.GetAssembly(typeof(Program)).Location);
+
+        static void Main()
+        {
+            var connectionString = ConnectionStringHelper.GetConnectionString("TestDB");
+            Console.WriteLine("Your ConnectionString is: {0}", connectionString);
+            var scriptsDir = Path.Combine(rootDirectory, "Scripts");
+
+            var processInfo = new ProcessStartInfo(GetRoundHouseFileName())
+            {
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                Arguments = "/f=" + scriptsDir + " /vf=BuildInfo /c=" + connectionString + " --ni",
+                WorkingDirectory = Path.GetDirectoryName(GetRoundHouseFileName())
+            };
+
+            var exitCode = 0;
+
+            using (var process = Process.Start(processInfo))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                var input = process.StandardInput;
+                var output = process.StandardOutput;
+
+                Console.WriteLine(output.ReadLine());
+                Console.WriteLine(output.ReadLine());
+                Console.WriteLine(output.ReadLine());
+
+#if DEBUG
+                Console.WriteLine("Press enter to Continue");
+                input.Write(Console.ReadLine());
+                input.Close();
+#endif
+                Console.WriteLine(output.ReadToEnd());
+#if DEBUG
+                Console.ReadLine();
+#endif
+                process.WaitForExit();
+                exitCode = process.ExitCode;
+                process.Close();
+            }
+
+            Environment.ExitCode = exitCode;
+        }
+        private static string GetRoundHouseFileName()
+        {
+            return Path.Combine(rootDirectory, "RoundhousE", "rh.exe");
+        }
+
+    }
+}
